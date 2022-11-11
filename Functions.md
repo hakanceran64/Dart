b7d7eef87c6cde1a2cd77e5b4ea811edd7156d1e
Hakan CERAN <hakanceran64@gmail.com>
2022-11-11 15:31:19 +0300

    Function

--- Functions.md
+++ Functions.md
@@ -1,1 +1,15 @@
 # Functions
+
+~~~Dart
+
+    return_type func_name (parameter_list) {
+        // statement [1]
+        // .
+        // .
+        // .
+        // statement [n]
+
+        return value;
+    }
+
+~~~
--- main.dart
+++ main.dart
@@ -1,3 +1,18 @@
 // ignore_for_file: unused_local_variable
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  var command = "OPEN";
+  switch (command) {
+    case "OPEN":
+      print("OPEN");
+      continue nowClosed;
+    case "CLOSED":
+      print("CLOSED");
+      break;
+    nowClosed:
+    case "NOW_CLOSED":
+      print("Now Closed");
+      break;
+    default:
+  }
+}
